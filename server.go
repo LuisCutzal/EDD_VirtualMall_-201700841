09232b825cd8bc24dba96e7fd4870ebe0c338be1
LuisCutzal <70243302+LuisCutzal@users.noreply.github.com>
2021-02-10 15:01:08 -0600

    funciona
    los metodos y estructuras funcionan, ademas de que se modifico el archivo de entrada de prueba

--- server.go
+++ server.go
@@ -3,20 +3,23 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
 type Tienda struct {
-	Nombre string
-	Descripcion string
-	Contacto string
+	Nombre       string
+	Descripcion  string
+	Contacto     string
 	Calificacion int
 }
 type Departamento struct {
-	Nombre string
+	Nombre  string
 	Tiendas []Tienda
+
 }
 
 type Dato struct {
@@ -32,26 +35,25 @@ func main() {
 	request()
 }
 
-func request(){
-	ruta:=mux.NewRouter().StrictSlash(true)//es para enlazar diferentes peticones, get, post, put, delete
+func request() {
+	ruta := mux.NewRouter().StrictSlash(true) //es para enlazar diferentes peticones, get, post, put, delete
 	ruta.HandleFunc("/", home)
-	ruta.HandleFunc("/getArreglo",getArreglo).Methods("GET")//se quedara esperando una respuesta, si no la tiene dara error
-	ruta.HandleFunc("/metodopost", metodopost).Methods("POST")//no se queda esperando una respuesta, solo mandamos informacion
-	log.Fatal(http.ListenAndServe(":3000",ruta))
+	ruta.HandleFunc("/getArreglo", getArreglo).Methods("GET")  //se quedara esperando una respuesta, si no la tiene dara error
+	ruta.HandleFunc("/metodopost", metodopost).Methods("POST") //no se queda esperando una respuesta, solo mandamos informacion
+	log.Fatal(http.ListenAndServe(":3000", ruta))
 
 }
-func home(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprint(w,"Servidor en go")
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Servidor en go")
 }
 
-func getArreglo(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprint(w,"[1,2,3,4,5]")// lo q tenemos que mandar esta en formato json
+func getArreglo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "[1,2,3,4,5]") // lo q tenemos que mandar esta en formato json
 
 }
-func metodopost(w http.ResponseWriter, r *http.Request){
+func metodopost(w http.ResponseWriter, r *http.Request) {
 	body, _ := ioutil.ReadAll(r.Body)
 	var re Sobre
 	json.Unmarshal(body, &re)
 	fmt.Println(re)
 }
-
